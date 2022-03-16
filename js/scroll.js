a5e1089fd3d95a0beee44102ef2d8879d3d76d68
AndyR <marlon_rivera95@hotmail.com>
2022-03-16 12:26:11 -0500

    mas cambiosssss

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,4 @@
 ScrollReveal().reveal('.header__fixed__items', { delay: 200 });
-ScrollReveal().reveal('.titulo__logo', { delay: 200 });
-ScrollReveal().reveal('.titulo__opiniones', { delay: 200 });
-ScrollReveal().reveal('.titulo__mapa', { delay: 200 });
-ScrollReveal().reveal('.redes__footer', { delay: 200 });
 
 
 window.sr = ScrollReveal();
